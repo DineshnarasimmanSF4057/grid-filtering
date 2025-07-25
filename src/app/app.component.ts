f94bddce3636f4b1c5904f3b0909903078fee252
DineshnarasimmanMSF4057 <125851531+DineshnarasimmanMSF4057@users.noreply.github.com>
2025-07-25 16:56:45 +0530

    commiting code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  GridAllModule,
+} from '@syncfusion/ej2-angular-grids';
 
+import { updatedHierarchyOrderData } from './data';
+import { isDevMode } from '@angular/core';
 @Component({
   selector: 'app-root',
+  templateUrl: 'app.component.html',
+  providers: [ ],
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [GridAllModule],
 })
 export class AppComponent {
-  title = 'my-grid-filtering-app';
+   public data: Object[] = [];
+  public filterSettings?: Object;
+  public toolbar?: string[];
+  public editSettings?: Object;
+  public orderidrules?: Object;
+  public customeridrules?: Object;
+  public freightrules?: Object;
+  ngOnInit(): void {
+     if (!isDevMode()) {
+    console.log('Production mode!');
+  } else {
+    console.log('Development mode!');
+  }
+    this.data = updatedHierarchyOrderData;
+    this.filterSettings = {
+      mode: 'Immediate',
+      showFilterBarOperator: true,
+      showFilterBarStatus: false,
+      enableCaseSensitivity: false, // This is explicitly set to false
+      operators: {},
+    };
+    this.toolbar = ['Add', 'Edit', 'Delete', 'Update', 'Cancel'];
+    this.editSettings = {
+      allowEditing: true,
+      allowAdding: true,
+      allowDeleting: true,
+    };
+    this.orderidrules = { required: true, number: true };
+    this.customeridrules = { required: true, minLength: 5 };
+    this.freightrules = { required: true, min: 0 };
+  }
 }
